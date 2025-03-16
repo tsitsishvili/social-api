29ec2b39156d8c9ef817c292ccb4778f3ef1a155
tsitsishvili <torniketsitsishvili@gmail.com>
2025-03-16 22:55:21 +0400

    Update main.go

--- cmd/migrate/seed/main.go
+++ cmd/migrate/seed/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/tsitsishvili/social/internal/db"
 	"github.com/tsitsishvili/social/internal/env"
 	"github.com/tsitsishvili/social/internal/store"
@@ -8,6 +9,8 @@ import (
 )
 
 func main() {
+	godotenv.Load()
+
 	addr := env.GetString("DB_DSN", "postgres://postgres:root@localhost:5432/social?sslmode=disable")
 
 	conn, err := db.New(addr, 3, 3, "15m")
